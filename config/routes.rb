8c6379e391ae2305664c579cb637ac8b3095de75
Sky B <skyburn5616@gmail.com>
2021-03-03 17:57:27 -0800

    added models controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'subs#index'
+
+  # get '/subs', to: 'subs#index'
+  # get '/subs', to: 'subs#show'
+  # get '/subs/:id', to: 'subs#edit'
+
+  resources :subs do
+  resources :topics 
+  end
+  # resources :topics do 
+  #   resources :comments
+  # end
 end
