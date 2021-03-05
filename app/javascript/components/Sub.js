82909015996ceb06cde79c01548b7e696ec11448
Sky B <skyburn5616@gmail.com>
2021-03-04 17:38:47 -0800

    sub full crud

---
+++ app/javascript/components/Sub.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const Sub = ({ sub }) => {
+  const { id, title, created_at } = sub;
+  // const id = sub.id
+  // const title = sub.title
+  // const created_at = sub.created_at
+  return (
+    <>
+      <h1>Sub: {title}</h1>
+      <p>
+        created: {created_at}, id: {id}
+      </p>
+      <a href="/">back</a>
+    </>
+  )
+}
+export default Sub;
